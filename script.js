bee7e889d2ad162343b6de82a21a30e0d5cde97b
unknown <Parth10100402>
2026-02-20 10:34:20 +0530

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log("i am parth sharma")
+// console.log("hello world")
+// document.write("hey there parth")
+
+var num = 10
+console.log(num);
+console.log(typeof num);
+
+var num = false
+console.log(num);
+console.log(typeof num);
+
+var num = "parth"
+console.log(num);
+console.log(typeof num);
+
+var num = null
+console.log(num);
+console.log(typeof num);
+
+var num = undefined ;
+console.log(num);
+console.log(typeof num);
+
+
+let sayHello = function(name){
+    alert("hello " + name)
+}
+
+let numberArray = [1, 2 , 3 , 4 , 5]
+
+let animals = new Array("dog", "cat", "elephant"/ "lion");
+
+let person = {
+    name: "parth",
+    age : 20,
+    city : "gurgaon"
+}
+
+const symbl = symbol(4);
+console.log(symbl2);
+
+console.log(sumbl === sumbl2);
+
+if(symbl === sumbl2){
+    console.log("true");
+}
+
+else{
+    console.log("false");
+}
+
+let a = 100, b = 13 , c = 10;
+var linebreak = "<br>"
+document.write("a + b + c = " + (a + b + c) + "<br>");
+
+result = a + b + c;
+
+document.write("the sum of " + a + " , " + b + " and " + c + "is: " + result);
+document.write(linebreak);
