a3ee9e99cfe186bc58d491601e14f78006b53a30
unknown <Parth10100402>
2026-03-20 09:42:08 +0530

    object code

--- script.js
+++ script.js
@@ -1,72 +1,84 @@
 // console.log("hello world")
 // document.write("hey there parth")
 
-var num = 10
-console.log(num);
-console.log(typeof num);
+// var num = 10
+// console.log(num);
+// console.log(typeof num);
 
-var num = false
-console.log(num);
-console.log(typeof num);
+// var num = false
+// console.log(num);
+// console.log(typeof num);
 
-var num = "parth"
-console.log(num);
-console.log(typeof num);
+// var num = "parth"
+// console.log(num);
+// console.log(typeof num);
 
-var num = null
-console.log(num);
-console.log(typeof num);
+// var num = null
+// console.log(num);
+// console.log(typeof num);
 
-var num = undefined ;
-console.log(num);
-console.log(typeof num);
+// var num = undefined ;
+// console.log(num);
+// console.log(typeof num);
 
 
-let sayHello = function(name){
-    alert("hello " + name)
-}
+// let sayHello = function(name){
+//     alert("hello " + name)
+// }
 
-let numberArray = [1, 2 , 3 , 4 , 5]
+// let numberArray = [1, 2 , 3 , 4 , 5]
 
-let animals = new Array("dog", "cat", "elephant"/ "lion");
+// let animals = new Array("dog", "cat", "elephant"/ "lion");
 
-let person = {
-    name: "parth",
-    age : 20,
-    city : "gurgaon"
-}
+// let person = {
+//     name: "parth",
+//     age : 20,
+//     city : "gurgaon"
+// }
 
-const symbl = symbol(4);
-console.log(symbl2);
+// const symbl = symbol(4);
+// console.log(symbl2);
 
-console.log(sumbl === sumbl2);
+// console.log(sumbl === sumbl2);
 
-if(symbl === sumbl2){
-    console.log("true");
-}
+// if(symbl === sumbl2){
+//     console.log("true");
+// }
 
-else{
-    console.log("false");
-}
+// else{
+//     console.log("false");
+// }
 
-let a = 100, b = 13 , c = 10;
-var linebreak = "<br>"
-document.write("a + b + c = " + (a + b + c) + "<br>");
+// let a = 100, b = 13 , c = 10;
+// var linebreak = "<br>"
+// document.write("a + b + c = " + (a + b + c) + "<br>");
 
-result = a + b + c;
+// result = a + b + c;
 
-document.write("the sum of " + a + " , " + b + " and " + c + "is: " + result);
-document.write(linebreak);
+// document.write("the sum of " + a + " , " + b + " and " + c + "is: " + result);
+// document.write(linebreak);
 
 
 //promise represenrs the  result of an asyncronous operation
 
-var promise = new promise((resolve,reject) => {
-    let success = true
-    if(success)
-    resolve("data loaded");
-else
-    reject("error occured");
-});
-promise.then(result => console.log(result))
-  .catch(error => console.log(error));
+// var promise = new promise((resolve,reject) => {
+//     let success = true
+//     if(success)
+//     resolve("data loaded");
+// else
+//     reject("error occured");
+// });
+// promise.then(result => console.log(result))
+//   .catch(error => console.log(error));
+
+
+//storage object
+
+var user = {name:"parth" , age:50}
+localStorage.setltern("user",JSON.stringify(user));
+
+// retrieve Object:
+
+var data = JSON.parse(localStorage.getItem("user"));
+console.log(data.name);
+
